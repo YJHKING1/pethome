67e29cba8dfb811ec82cdbc3592dc4a2a5a2f4a9
YJHKING <yjhking@yjhking.org>
2022-07-03 13:57:26 +0800

    更新

--- src/main/java/org/yjhking/pethome/org/controller/DepartmentController.java
+++ src/main/java/org/yjhking/pethome/org/controller/DepartmentController.java
@@ -2,8 +2,10 @@ package org.yjhking.pethome.org.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.yjhking.pethome.basic.util.PageList;
 import org.yjhking.pethome.org.domain.Department;
 import org.yjhking.pethome.org.query.AjaxResult;
+import org.yjhking.pethome.org.query.DepartmentQuery;
 import org.yjhking.pethome.org.service.DepartmentService;
 
 import java.util.List;
@@ -33,8 +35,9 @@ public class DepartmentController {
     }
     
     @PutMapping("/{id}")
-    public AjaxResult editById(@RequestBody Department department) {
-        Long id = departmentService.updateByPrimaryKeySelective(department);
+    public AjaxResult editById(@RequestBody Department department, @PathVariable Long id) {
+        department.setId(id);
+        departmentService.updateByPrimaryKeySelective(department);
         return new AjaxResult();
     }
     
@@ -42,5 +45,10 @@ public class DepartmentController {
     public AjaxResult add(@RequestBody Department department) {
         Long id = departmentService.insertSelective(department);
         return new AjaxResult();
+    }
+    
+    @PostMapping
+    public PageList<Department> queryPage(@RequestBody DepartmentQuery departmentQuery) {
+        return departmentService.queryData(departmentQuery);
     }
 }
