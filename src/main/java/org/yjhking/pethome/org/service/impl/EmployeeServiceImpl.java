90dc8d938808c1232768ef7f44d53396806417f9
YJHKING <yjhking@yjhking.org>
2022-07-16 10:08:26 +0800

    更新

--- src/main/java/org/yjhking/pethome/org/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/yjhking/pethome/org/service/impl/EmployeeServiceImpl.java
@@ -1,12 +1,17 @@
 package org.yjhking.pethome.org.service.impl;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yjhking.pethome.basic.service.impl.BaseServiceImpl;
+import org.yjhking.pethome.basic.util.Md5Utils;
+import org.yjhking.pethome.basic.util.StrUtils;
 import org.yjhking.pethome.org.domain.Employee;
 import org.yjhking.pethome.org.mapper.EmployeeMapper;
 import org.yjhking.pethome.org.service.EmployeeService;
+import org.yjhking.pethome.user.domain.Logininfo;
+import org.yjhking.pethome.user.mapper.LogininfoMapper;
 
 /**
  * 部门业务层
@@ -17,24 +22,51 @@ import org.yjhking.pethome.org.service.EmployeeService;
 public class EmployeeServiceImpl extends BaseServiceImpl<Employee> implements EmployeeService {
     @Autowired
     private EmployeeMapper employeeMapper;
+    @Autowired
+    private LogininfoMapper logininfoMapper;
     
     @Transactional
     @Override
     public Integer insertSelective(Employee employee) {
+        String salt = StrUtils.getComplexRandomString(32);
+        String password = Md5Utils.encrypByMd5(employee.getPassword() + salt);
+        employee.setSalt(salt);
+        employee.setPassword(password);
+        Logininfo logininfo = employee2Logininfo(employee);
+        logininfoMapper.insertSelective(logininfo);
+        employee.setLogininfoId(logininfo.getId());
         // 新增到数据库
-        employeeMapper.insertSelective(employee);
-        // 将登录id设为新增的id
-        employee.setLogininfoId(employee.getId());
-        // 更新
-        return employeeMapper.updateByPrimaryKeySelective(employee);
+        return employeeMapper.insertSelective(employee);
     }
     
     @Transactional
     @Override
     public Integer updateByPrimaryKeySelective(Employee employee) {
-        // 将登录id设为id
-        employee.setLogininfoId(employee.getId());
+        String salt = StrUtils.getComplexRandomString(32);
+        String password = Md5Utils.encrypByMd5(employee.getPassword() + salt);
+        employee.setSalt(salt);
+        employee.setPassword(password);
+        Logininfo logininfo = employee2Logininfo(employee);
+        // 查询员工的登录信息id
+        Long logininfoId = employeeMapper.selectByPrimaryKey(employee.getId()).getLogininfoId();
+        logininfo.setId(logininfoId);
+        logininfoMapper.updateByPrimaryKeySelective(logininfo);
         // 更新到数据库
         return employeeMapper.updateByPrimaryKeySelective(employee);
+    }
+    
+    private Logininfo employee2Logininfo(Employee employee) {
+        Logininfo logininfo = new Logininfo();
+        BeanUtils.copyProperties(employee, logininfo);
+        logininfo.setType(0);
+        return logininfo;
+    }
+    
+    @Override
+    public Integer deleteByPrimaryKey(Long id) {
+        Employee employee = employeeMapper.selectByPrimaryKey(id);
+        //删除登录信息表数据
+        logininfoMapper.deleteByPrimaryKey(employee.getLogininfoId());
+        return super.deleteByPrimaryKey(id);
     }
 }
